39284a8142b2d3469bc58200f18690b5976b515c
Fvh092024 <fvh092024@gmail.com>
2025-01-14 16:01:06 +0100

    Add files via upload
    Finished pieces, changed background-color and movable element color.

---
+++ script.js
@@ -0,0 +1,54 @@
+const flyttmeg = document.getElementById("flyttmeg-btn");
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
+const tetI = document.querySelector(".tetris-piece-i-shape");
+// const tetO = document.querySelector(".tetris-piece-o-shape");
+
+// flyttmeg.setAttribute("tabindex", "0");
+// flyttmeg.focus();
+window.onload = function () {
+  flyttmeg.setAttribute("tabindex", "0");
+  flyttmeg.focus();
+};
+flyttmeg.addEventListener("keydown", function (event) {
+  console.log("Key pressed:", event.key);
+  let top = parseInt(window.getComputedStyle(flyttmeg).top) || 0;
+  let left = parseInt(window.getComputedStyle(flyttmeg).left) || 0;
+
+  switch (event.key) {
+    case "ArrowUp":
+      if (top - 10 >= 0) {
+        flyttmeg.style.top = top - 10 + "px";
+      }
+      break;
+    case "ArrowDown":
+      if (top + 10 + flyttmeg.offsetHeight <= viewportHeight) {
+        flyttmeg.style.top = top + 10 + "px";
+      }
+      break;
+    case "ArrowLeft":
+      if (left - 10 >= 0) {
+        flyttmeg.style.left = left - 10 + "px";
+      }
+      break;
+    case "ArrowRight":
+      if (left + 10 + flyttmeg.offsetWidth <= viewportWidth) {
+        flyttmeg.style.left = left + 10 + "px";
+      }
+      break;
+  }
+});
+document.addEventListener("click", function (event) {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  const borderLeft = mouseX - flyttmeg.offsetWidth / 2;
+  const borderTop = mouseY - flyttmeg.offsetHeight / 2;
+
+  if (borderLeft >= 0 && borderLeft + flyttmeg.offsetWidth <= viewportWidth) {
+    flyttmeg.style.left = borderLeft + "px";
+  }
+  if (borderTop >= 0 && borderTop + flyttmeg.offsetHeight <= viewportHeight) {
+    flyttmeg.style.top = borderTop + "px";
+  }
+});
